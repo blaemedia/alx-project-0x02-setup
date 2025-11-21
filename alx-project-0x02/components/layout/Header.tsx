eee2ed19d87f30d97d9a4cf51a6337af23de5cad
Emmanuel <blaemedia26@gmail.com>
2025-11-21 20:15:10 +0100

    Set Up a Next.js Project

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -7,8 +7,8 @@ export default function Header(){
         <header className="bg-blue-100 shadow-md ">
             <nav className="flex gap-4">
                 <Link href="/home" className="hover:underline text-blue-600">Home</Link>
-                <Link href="/home" className="hover:underline text-blue-600">About</Link>
-                <Link href="/home" className="hover:underline text-blue-600">posts</Link>
+                <Link href="/about" className="hover:underline text-blue-600">About</Link>
+                <Link href="/post" className="hover:underline text-blue-600">posts</Link>
             </nav>
         </header>
     );
